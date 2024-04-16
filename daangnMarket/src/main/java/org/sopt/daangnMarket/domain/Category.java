d703b1a1d6f263d4f9aa3bafcbb6b4c959d6e698
chaewonkim <kimlove9182@gmail.com>
2024-04-16 19:31:40 +0900

    feat: Category 엔티티 생성

---
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/domain/Category.java
@@ -0,0 +1,18 @@
+package org.sopt.daangnMarket.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
