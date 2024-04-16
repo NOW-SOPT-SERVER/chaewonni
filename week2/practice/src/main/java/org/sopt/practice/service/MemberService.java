5c45915f0beaed5c0c88c0b4bf668518e9752c1c
chaewonkim <kimlove9182@gmail.com>
2024-04-16 14:39:33 +0900

    feat: 2차 세미나 과제

---
+++ week2/practice/src/main/java/org/sopt/practice/service/MemberService.java
@@ -0,0 +1,49 @@
+package org.sopt.practice.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.sopt.practice.domain.Member;
+import org.sopt.practice.repository.MemberRepository;
+import org.sopt.practice.service.dto.MemberCreateDto;
+import org.sopt.practice.service.dto.MemberFindDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional //db에 변경사항이 생길 때 사용
+    public String createMember(MemberCreateDto memberCreate) {
+
+        Member member = Member.create(memberCreate.name(), memberCreate.part(), memberCreate.age());
+        memberRepository.save(member);
+        return member.getId().toString();
+    }
+
+    public MemberFindDto findMemberById(Long memberId) {
+        return MemberFindDto.of(memberRepository.findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException("ID에 해당하는 사용자가 존재하지 않습니다.")
+        ));
+    }
+
+    @Transactional
+    public void deleteMemberById(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException("ID에 해당하는 사용자가 존재하지 않습니다."));
+        memberRepository.delete(member);
+    }
+
+    public List<MemberFindDto> findAllMembers() {
+        List<Member> members = memberRepository.findAll();
+        return members.stream()
+                .map(MemberFindDto::of)
+                .collect(Collectors.toList());
+    }
+}
