de8bf28297f260b9e00340bd7ad62014d2bd614b
chaewonkim <kimlove9182@gmail.com>
2024-04-16 18:39:11 +0900

    feat: Member 엔티티 및 정적 팩토리 메서드 추가

---
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/domain/Member.java
@@ -0,0 +1,33 @@
+package org.sopt.daangnMarket.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private double MannerTemperature;
+
+    // 정적 팩토리 메서드
+    public static Member create(String nickname, String phoneNumber, double MannerTemperature) {
+        Member member = new Member();
+        member.nickname = nickname;
+        member.phoneNumber = phoneNumber;
+        member.MannerTemperature = MannerTemperature;
+        return member;
+    }
+}
