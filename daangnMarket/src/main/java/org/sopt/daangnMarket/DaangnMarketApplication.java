84090fca9cf1228abb4c17d92cdd4aaa58b6d85e
chaewonkim <kimlove9182@gmail.com>
2024-04-16 17:57:41 +0900

    feat: 초기 세팅

---
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/DaangnMarketApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.daangnMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnMarketApplication.class, args);
+	}
+
+}
---
+++ daangnMarket/src/test/java/org/sopt/daangnMarket/DaangnMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.daangnMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
