8f9e940f98f094d9e1bae0514a34fc0a47ecc302
chaewonkim <kimlove9182@gmail.com>
2024-04-29 16:57:30 +0900

    feat: 게시글 전체 조회

--- week3/practice/src/main/java/org/sopt/practice/common/dto/SuccessMessage.java
+++ week3/practice/src/main/java/org/sopt/practice/common/dto/SuccessMessage.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum SuccessMessage {
 
-    BLOG_CREATE_SUCCESS(HttpStatus.CREATED.value(),"블로그 생성이 완료되었습니다.");
+    BLOG_CREATE_SUCCESS(HttpStatus.CREATED.value(),"블로그 생성이 완료되었습니다."),
+    POST_CREATE_SUCCESS(HttpStatus.CREATED.value(),"게시글 생성이 완료되었습니다."),
+
+    POST_FIND_SUCCESS(HttpStatus.OK.value(), "게시글 찾기가 완료되었습니다.");
 
     private final int status;
     private final String message;
--- week3/practice/src/main/java/org/sopt/practice/common/dto/SuccessStatusResponse.java
+++ week3/practice/src/main/java/org/sopt/practice/common/dto/SuccessStatusResponse.java
@@ -1,13 +1,20 @@
 package org.sopt.practice.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
-public record SuccessStatusResponse(
+public record SuccessStatusResponse<T>(
         int status,
-        String message
+        String message,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        T data
 ) {
 
-    public static SuccessStatusResponse of(SuccessMessage successMessage) {
-        return new SuccessStatusResponse(successMessage.getStatus(),successMessage.getMessage());
+    public static SuccessStatusResponse<Void> of(SuccessMessage successMessage) {
+        return new SuccessStatusResponse<Void>(successMessage.getStatus(),successMessage.getMessage(), null);
+    }
+
+    public static <T> SuccessStatusResponse<T> of(SuccessMessage successMessage, T data) {
+        return new SuccessStatusResponse<T>(successMessage.getStatus(),successMessage.getMessage(),data);
     }
 }
