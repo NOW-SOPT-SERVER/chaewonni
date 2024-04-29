7c7031d862fd30dc3cdd83e773abe095d99e36d0
chaewonkim <kimlove9182@gmail.com>
2024-04-29 16:57:01 +0900

    feat: 게시글 단건 조회

---
+++ week3/practice/src/main/java/org/sopt/practice/service/dto/PostFindDto.java
@@ -0,0 +1,18 @@
+package org.sopt.practice.service.dto;
+
+import org.sopt.practice.domain.Blog;
+import org.sopt.practice.domain.Post;
+
+public record PostFindDto(
+        String title,
+        String content,
+        BlogFindDto blog
+) {
+    public static PostFindDto of(Post post){
+        return new PostFindDto(
+                post.getTitle(),
+                post.getContent(),
+                BlogFindDto.of(post.getBlog())
+        );
+    }
+}
