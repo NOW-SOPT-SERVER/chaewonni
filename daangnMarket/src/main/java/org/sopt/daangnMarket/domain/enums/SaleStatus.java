b690bad86e34fe648fd90fc00d498b93e95c2449
chaewonkim <kimlove9182@gmail.com>
2024-04-29 12:42:40 +0900

    refactor: 판매 상태 enum class 한글 추가

--- daangnMarket/src/main/java/org/sopt/daangnMarket/domain/enums/SaleStatus.java
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/domain/enums/SaleStatus.java
@@ -1,8 +1,26 @@
 package org.sopt.daangnMarket.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum SaleStatus {
 
-    FOR_SALE,
-    RESERVED,
-    SOLD_OUT
+    FOR_SALE("판매중"),
+    RESERVED("예약중"),
+    SOLD_OUT("거래완료");
+
+    private final String koreanName;
+
+    SaleStatus(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public static SaleStatus fromKoreanName(String koreanName) {
+        for (SaleStatus saleStatus : SaleStatus.values()) {
+            if (saleStatus.getKoreanName().equals(koreanName)) {
+                return saleStatus;
+            }
+        }
+        return null;
+    }
 }
