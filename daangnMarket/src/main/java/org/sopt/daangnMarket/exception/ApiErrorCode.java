34991eb1a3479ea43848923ec732f20d96810a24
chaewonkim <kimlove9182@gmail.com>
2024-04-30 13:08:52 +0900

    refactor: 예외 메시지 수정 및 보완

--- daangnMarket/src/main/java/org/sopt/daangnMarket/exception/ApiErrorCode.java
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/exception/ApiErrorCode.java
@@ -4,11 +4,14 @@ import org.springframework.http.HttpStatus;
 
 public enum ApiErrorCode {
 
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Member not found"),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Category not found"),
-    LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Location not found"),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 사용자가 존재하지 않습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 카테고리가 존재하지 않습니다."),
+    LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 동네가 존재하지 않습니다."),
+    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 상품이 존재하지 않습니다."),
 
-    DUPLICATE_MEMBER(HttpStatus.CONFLICT.value(), "Duplicate member"),
+    DUPLICATE_MEMBER(HttpStatus.CONFLICT.value(), "이미 가입한 회원입니다."),
+
+    BOOKMARK_CONFLICT(HttpStatus.CONFLICT.value(), "이미 좋아요를 누르셨습니다."),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal server error");
 
