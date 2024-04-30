355d922d9d6eb6f151ed0b6ebd1baaf3c7b764af
chaewonkim <kimlove9182@gmail.com>
2024-04-30 11:39:05 +0900

    feat: Member에 사용자가 인증한 현재 동네 추가

--- daangnMarket/src/main/java/org/sopt/daangnMarket/dto/request/member/MemberCreateDto.java
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/dto/request/member/MemberCreateDto.java
@@ -9,6 +9,8 @@ public record MemberCreateDto(
         @Size(min = 2, max = 10, message = "닉네임은 2글자 이상, 10글자 이하여야 합니다.")
         String nickname,
         @NotBlank(message = "핸드폰 번호는 필수 입력 사항입니다.")
-        String phoneNumber
+        String phoneNumber,
+        @NotBlank(message = "사용자 동네 인증은 필수 사항입니다.")
+        String location
 ) {
 }
--- daangnMarket/src/main/java/org/sopt/daangnMarket/dto/response/member/MemberFindDto.java
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/dto/response/member/MemberFindDto.java
@@ -1,17 +1,20 @@
 package org.sopt.daangnMarket.dto.response.member;
 
+import org.sopt.daangnMarket.domain.Location;
 import org.sopt.daangnMarket.domain.Member;
 
 public record MemberFindDto(
 
         Long id,
         String nickname,
+        Location location,
         double mannerTemperature
 ) {
     public static MemberFindDto of(Member member) {
         return new MemberFindDto(
                 member.getId(),
                 member.getNickname(),
+                member.getLocation(),
                 member.getMannerTemperature());
     }
 }
