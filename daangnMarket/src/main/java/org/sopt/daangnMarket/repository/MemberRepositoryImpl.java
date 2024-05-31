6f090fcd61641074f078afbe94d9a0005097c6b1
chaewonkim <kimlove9182@gmail.com>
2024-05-31 16:11:35 +0900

    chore: querydsl 잠시 주석..

--- daangnMarket/src/main/java/org/sopt/daangnMarket/repository/MemberRepositoryImpl.java
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/repository/MemberRepositoryImpl.java
@@ -1,24 +1,24 @@
-package org.sopt.daangnMarket.repository;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-
-import static org.sopt.daangnMarket.domain.QMember.*;
-
-public class MemberRepositoryImpl implements MemberRepositoryCustom {
-    private final JPAQueryFactory queryFactory;
-
-    public MemberRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
-    }
-
-    @Override
-    public Long countByPhoneNumber(String phoneNumber) {
-        return queryFactory
-                .select(member.count())
-                .from(member)
-                .where(member.phoneNumber.eq(phoneNumber))
-                .fetchOne();
-    }
-
-}
+//package org.sopt.daangnMarket.repository;
+//
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import jakarta.persistence.EntityManager;
+//
+//import static org.sopt.daangnMarket.domain.QMember.*;
+//
+//public class MemberRepositoryImpl implements MemberRepositoryCustom {
+//    private final JPAQueryFactory queryFactory;
+//
+//    public MemberRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    }
+//
+//    @Override
+//    public Long countByPhoneNumber(String phoneNumber) {
+//        return queryFactory
+//                .select(member.count())
+//                .from(member)
+//                .where(member.phoneNumber.eq(phoneNumber))
+//                .fetchOne();
+//    }
+//
+//}
