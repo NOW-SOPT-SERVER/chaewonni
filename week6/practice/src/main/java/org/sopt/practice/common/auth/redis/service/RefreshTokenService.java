927a553fe5953cb7765bee78339e94f05dce7bde
chaewonkim <kimlove9182@gmail.com>
2024-06-02 23:25:22 +0900

    refactor: RefreshTokenService 수정

--- week6/practice/src/main/java/org/sopt/practice/common/auth/redis/service/RefreshTokenService.java
+++ week6/practice/src/main/java/org/sopt/practice/common/auth/redis/service/RefreshTokenService.java
@@ -2,13 +2,16 @@ package org.sopt.practice.common.auth.redis.service;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.practice.common.auth.UserAuthentication;
+import org.sopt.practice.common.auth.dto.CustomUserDetails;
 import org.sopt.practice.common.auth.redis.domain.Token;
 import org.sopt.practice.common.auth.redis.repository.TokenRepository;
 import org.sopt.practice.common.dto.ErrorMessage;
 import org.sopt.practice.common.jwt.JwtTokenProvider;
 import org.sopt.practice.common.jwt.JwtValidationType;
+import org.sopt.practice.domain.Member;
 import org.sopt.practice.exception.NotFoundException;
 import org.sopt.practice.exception.UnauthorizedException;
+import org.sopt.practice.repository.MemberRepository;
 import org.sopt.practice.service.dto.ReissueTokenResponse;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +21,7 @@ public class RefreshTokenService {
 
     private final TokenRepository tokenRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
 
     public ReissueTokenResponse reissueToken(String refreshToken) {
         if(jwtTokenProvider.validateToken(refreshToken) != JwtValidationType.VALID_JWT) {
@@ -29,8 +33,12 @@ public class RefreshTokenService {
             throw new UnauthorizedException(ErrorMessage.TOKEN_INVALID_ERROR);
         }
 
-        UserAuthentication userAuthentication = UserAuthentication.createUserAuthentication(memberId);
-        return ReissueTokenResponse.of(jwtTokenProvider.issueAccessToken(userAuthentication));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND));
+
+        CustomUserDetails userDetails = new CustomUserDetails(member);
+
+        return ReissueTokenResponse.of(jwtTokenProvider.issueAccessToken(userDetails));
     }
 
     public Long findIdByRefreshToken(String refreshToken) {
