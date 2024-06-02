660cf2d27909b1f9da59454c02614435e3ef6b90
chaewonkim <kimlove9182@gmail.com>
2024-06-02 23:24:59 +0900

    refactor: SecurityConfig 수정

--- week6/practice/src/main/java/org/sopt/practice/common/auth/SecurityConfig.java
+++ week6/practice/src/main/java/org/sopt/practice/common/auth/SecurityConfig.java
@@ -4,12 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.sopt.practice.common.auth.filter.CustomAccessDeniedHandler;
 import org.sopt.practice.common.auth.filter.CustomJwtAuthenticationEntryPoint;
 import org.sopt.practice.common.auth.filter.JwtAuthenticationFilter;
+import org.sopt.practice.common.auth.filter.LoginFilter;
+import org.sopt.practice.common.auth.service.AuthenticationService;
+import org.sopt.practice.common.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,12 +23,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 @EnableWebSecurity //web Security를 사용할 수 있게
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationService authenticationService;
     private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
 
-    private static final String[] AUTH_WHITE_LIST = {"/api/v1/members", "/api/v1/auth/refresh"};
+    private static final String[] AUTH_WHITE_LIST = {"/api/v1/members", "/api/v1/auth/refresh", "/api/v1/members/login"};
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,10 +47,26 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(auth -> {
                     auth.requestMatchers(AUTH_WHITE_LIST).permitAll();
+                    auth.requestMatchers("/admin").hasRole("ADMIN");
                     auth.anyRequest().authenticated();
                 })
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), authenticationService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
