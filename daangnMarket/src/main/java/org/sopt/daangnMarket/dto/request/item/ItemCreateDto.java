6993f010c2c75b952e0d29e45426345eae34fb7e
chaewonkim <kimlove9182@gmail.com>
2024-06-11 22:11:06 +0900

    chore: @NotBlank 삭제

--- daangnMarket/src/main/java/org/sopt/daangnMarket/dto/request/item/ItemCreateDto.java
+++ daangnMarket/src/main/java/org/sopt/daangnMarket/dto/request/item/ItemCreateDto.java
@@ -12,8 +12,7 @@ public record ItemCreateDto(
         String category,
         String tradeType,
         int price,
-        @NotBlank(message = "자세한 설명은 필수 입력 사항입니다.")
-        @Size(min = 4, message = "최소 4글자 이상이어야 합니다.")
+        @Size(min = 4, message = "설명란은 최소 4글자 이상이어야 합니다.")
         String description,
         String address,
         MultipartFile image
